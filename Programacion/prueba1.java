c4859364f0a41ae50d85d637bba2f2bca0cf412e
MickSenpai <mick967@hotmail.com>
2025-08-07 10:25:56 -0700

    Error al momento de seleccionar planilla 2 (continua) y al contar votos

--- Programacion/prueba1.java
+++ Programacion/prueba1.java
@@ -60,19 +60,28 @@ public class prueba1 {
             System.out.println("-----------------------------------------------------");
             System.out.print  ("-- Por favor ingrese el password de administrador: ");
             System.out.println("-----------------------------------------------------");
-            
+
+            System.out.print("Password: ");
             String pwdUser = password.next();
             
-            if (pwdUser.equals(passwd)) {
+            if(pwdUser.trim().isEmpty()){
+                password.nextLine();
+                System.out.println("Por favor ingrese una contrasena");
+                @SuppressWarnings("unused")
+                String message = password.nextLine();
+
+                Admin_Loggin();
+            }else if (pwdUser.equals(passwd)) {
                 Admin();
             } else {
+                password.nextLine();
                 System.out.println();
                 System.out.println("Contrasena incorrecta, intentelo de nuevo.");
                 System.out.println("Presione ENTER para continuar");
                 @SuppressWarnings("unused")
                 String message = password.nextLine();
                 
-                Votante_Loggin();
+                Menu();
             }
         }
     }
@@ -88,16 +97,34 @@ public class prueba1 {
             //Manejo de error en caso de que agregue algo que no sean numeros.
             try {
                 System.out.print("Ingresa tu ID: "); //ID = Matricula
-                int id = matricula.nextInt();
-                
-                Listado_IDs(id); //Se manda a validacion en la funcion Listado_IDs
+                String id = matricula.nextLine();
+
+                if(id.trim().isEmpty()){
+                    System.out.println("No se ha ingresado ningun dato. Por favor, intente de nuevo.");
+                    Votante_Loggin();
+                } else {
+                    try {
+                        int idNumero = Integer.parseInt(id.trim());
+                        Listado_IDs(idNumero); //Se manda a validacion en la funcion Listado_IDs
+                    } catch (Exception e) {
+                        System.out.println();
+                        System.out.println("****************************************");
+                        System.out.println("Ha ocurrido un error... Intente de nuevo");
+                        System.out.println("---- Presione ENTER para continuar -----");
+                        System.out.println("****************************************");
+                        @SuppressWarnings("unused")
+                        String message = matricula.nextLine();
+
+                        Menu();
+                    }
+                }
             } catch (Exception e) {
                 matricula.nextLine();
                 
                 System.out.println();
                 System.out.println("******************************");
                 System.out.println("Por favor ingrese solo numeros");
-                System.out.println("Presiona ENTER para continuar");
+                System.out.println("Presione ENTER para continuar");
                 System.out.println("******************************");
                 @SuppressWarnings("unused")
                 String message = matricula.nextLine();
@@ -194,112 +221,99 @@ public class prueba1 {
 
     static ArrayList<ArrayList <String>>  planillaList = new ArrayList<>(); //Listado tiulos planillas (Global)
 
-    static void Agregar_Planillas(){
-
-        limpiarConsola();
-        try (Scanner planillas = new Scanner(System.in)) {
-            System.out.println("-----------------------------------");
-            System.out.println("------- Agregar Planillas   -------");
-            System.out.println("-----------------------------------");
-            System.out.println();
-            try {
-                System.out.println("Cuantas planillas desea agregar?");
-                int cantidadPlanillas = planillas.nextInt();
+   static void Agregar_Planillas() {
+    
+    limpiarConsola();
+    try (Scanner planillas = new Scanner(System.in)) {
+        System.out.println("-----------------------------------");
+        System.out.println("------- Agregar Planillas   -------");
+        System.out.println("-----------------------------------");
+        System.out.println();
+        try {
+            System.out.println("Cuantas planillas desea agregar?");
+            int cantidadPlanillas = planillas.nextInt();
+            
+            for(int p = 1; p <= cantidadPlanillas; p++){
+                ArrayList<String> Titulos = new ArrayList<>();
                 
-                for(int p = 1; p <= cantidadPlanillas; p++){
-                    ArrayList <String> Titulos = new ArrayList<>();
-                    
-                    planillas.nextLine();
-                    
-                    System.out.println("Planilla " + p);
-
-                    System.out.print("Ingrese el nombre de la planilla: "); //Posible a remover
-                    String nombrePlanilla = planillas.nextLine();
-
-                    int numIntegrantes;
-                    
-                    System.out.println("Cuantos integrantes habra en la planilla?");
-                    numIntegrantes = planillas.nextInt();
-                    planillas.nextLine();
-                    
-                    for(int i = 1; i <= numIntegrantes; i++){
-                        System.out.print("Ingrese el cargo del integrante " + i + ": ");
-                        String cargo = planillas.nextLine();
-
-                        
-                        Titulos.add(cargo);
-                    }
-                    
-                    System.out.println();
-                    
-                    for(int i = 0; i < Titulos.size(); i++){
-                        System.out.println("Ingrese el nombre del " + Titulos.get(i));
-                        String nombreIntegrante = planillas.nextLine();
-                        
-                        String cargoActual = Titulos.get(i);
-                        
-                        Titulos.set(i, cargoActual + ": " + nombreIntegrante);
-                    }
-                    
-                    planillaList.add(Titulos);
-                }
-
-                System.out.println();
-                
-                System.out.println("Planillas ingresadas: ");
-                for (int i = 0; i < planillaList.size(); i++) {
-                    System.out.println("Planilla " + (i + 1) + ": " + planillaList.get(i));
-                }
-                
-                System.out.println();
-                
-                System.out.println("Los datos son correctos?");
-                System.out.println("1- Si");
-                System.out.println("2- No");
-                System.out.print("Opcion: ");
-                int datosIngresados = planillas.nextInt();
-                
-                int guardarDatos = datosIngresados;
-
-                System.out.println();
-                
-                switch (guardarDatos) {
-                    case 1 :System.out.println("Datos guardados correctamente"); break;
-                    case 2 : System.out.println("Datos eliminados"); planillaList.removeAll(planillaList); break;
-                    default: System.out.println("Por favor, ingrese una opcion valida"); break;
-                }
-                
-                System.out.println();
-                
-                System.out.println("Le gustaria agregar otra planilla?");
-                System.out.println("1- Si");
-                System.out.println("2- No");
-                System.out.println("3- Eliminar planillas agregadas");
-                System.out.print("Opcion: ");
-                int agregarPlanilla = planillas.nextInt();
-                
-                switch (agregarPlanilla) {
-                    case 1 -> Agregar_Planillas();
-                    case 2 -> Admin();
-                    case 3 -> Eliminar_Planillas();
-                    default -> System.out.println("Ingrese una opcion valida");
-                }
-
-            } catch (Exception e) {
                 planillas.nextLine();
                 
-                System.out.println();
-                System.out.println("****************************");
-                System.out.println("Algo ha salido mal, por favor intentalo de nuevo...");
-                System.out.println("Presione ENTER para continuar");
-                System.out.println("****************************");
-                @SuppressWarnings("unused")
-                String repeticion = planillas.nextLine();
+                System.out.println("Planilla " + p);
                 
-                Agregar_Planillas();
+                System.out.print("Ingrese el nombre de la planilla: "); //Posible a remover
+                String nombrePlanilla = planillas.nextLine();
+                Titulos.add(nombrePlanilla); 
+
+                int numIntegrantes;
+
+                System.out.println("Cuantos integrantes habra en la planilla?");
+                numIntegrantes = planillas.nextInt();
+                planillas.nextLine();
+                
+                ArrayList<String> cargos = new ArrayList<>();
+                for(int i = 1; i <= numIntegrantes; i++){
+                    System.out.print("Ingrese el cargo del integrante " + i + ": ");
+                    String cargo = planillas.nextLine();
+                    cargos.add(cargo);
+                }
+                
+                System.out.println();
+                
+                for(int i = 0; i < numIntegrantes; i++){
+                    System.out.print("Ingrese el nombre para " + cargos.get(i) + ": ");
+                    String nombre = planillas.nextLine();
+                    Titulos.add(cargos.get(i) + ": " + nombre);  
+                }
+                
+                planillaList.add(Titulos);
             }
+
+            
+            System.out.println();
+            System.out.println("Los datos son correctos?");
+            System.out.println("1- Si");
+            System.out.println("2- No");
+            System.out.print("Opcion: ");
+            int datosIngresados = planillas.nextInt();
+            
+            int guardarDatos = datosIngresados;
+
+            System.out.println();
+            
+            switch (guardarDatos) {
+                case 1 :System.out.println("Datos guardados correctamente"); break;
+                case 2 : System.out.println("Datos eliminados"); planillaList.removeAll(planillaList); break;
+                default: System.out.println("Por favor, ingrese una opcion valida"); break;
+            }
+            
+            System.out.println();
+            
+            System.out.println("Le gustaria agregar otra planilla?");
+            System.out.println("1- Si");
+            System.out.println("2- No");
+            System.out.println("3- Eliminar planillas agregadas");
+            System.out.print("Opcion: ");
+            int agregarPlanilla = planillas.nextInt();
+            
+            switch (agregarPlanilla) {
+                case 1 -> Agregar_Planillas();
+                case 2 -> Admin();
+                case 3 -> Eliminar_Planillas();
+                default -> System.out.println("Ingrese una opcion valida");
+            }
+
+        } catch (Exception e) {
+            planillas.nextLine();
+            System.out.println();
+            System.out.println("****************************");
+            System.out.println("Algo ha salido mal, por favor intentalo de nuevo...");
+            System.out.println("Presione ENTER para continuar");
+            System.out.println("****************************");
+            String repeticion = planillas.nextLine();
+            Agregar_Planillas();
         }
     }
+}
 
     static void Eliminar_Planillas(){
         limpiarConsola();
@@ -360,34 +374,25 @@ public class prueba1 {
             System.out.println("Presiona ENTER para volver al menu...");
             integrantes.nextLine();
             Votante_Menu();
-            integrantes.close();
-            return;
         }
 
-        // Mostrar todas las planillas
-        for (int i = 0; i < planillaList.size(); i++) {
-            ArrayList<String> planillaActual = planillaList.get(i);
+        // Mostrar cada planilla
+        for (ArrayList<String> planilla : planillaList) {
+            // El primer elemento es el nombre de la planilla
+            System.out.println("\nplantilla [" + planilla.get(0) + "]");
             
-            // Obtener el nombre de la planilla (primer elemento)
-            String nombrePlanilla = planillaActual.get(0);
-            System.out.println("\nPlanilla [" + nombrePlanilla + "]");
-
-            for(int j = 1; j < planillaActual.size(); j++) {
-                String[] partes = planillaActual.get(j).split(": ");
-                if(partes.length > 1) {
-                    System.out.println("puesto" + j + " : " + partes[1]); 
-                } else {
-                    System.out.println("puesto" + j + " : Formato incorrecto");
-                }
+            // Mostrar cada cargo y persona (empezando desde el segundo elemento)
+            for (int j = 1; j < planilla.size(); j++) {
+                System.out.println(planilla.get(j));
             }
         }
         
-        System.out.println("-----------------------------------");
+        System.out.println("\n-----------------------------------");
         System.out.println("--- Presione ENTER para volver  ---");
         System.out.println("-----------------------------------");
 
         integrantes.nextLine();
-        Votante_Menu();
+        Votaciones();
 
         integrantes.close();
     }
@@ -405,11 +410,11 @@ public class prueba1 {
             
 
             for (int i = 0; i < planillaList.size(); i++) {
-            ArrayList<String> planillaActual = planillaList.get(i);
+                ArrayList<String> planillaActual = planillaList.get(i);
             
-            // Obtener el nombre de la planilla (primer elemento)
-            String nombrePlanilla = planillaActual.get(0);
-            System.out.println("\nPlanilla [" + nombrePlanilla + "]");
+                // Obtener el nombre de la planilla (primer elemento)
+                String nombrePlanilla = planillaActual.get(0);
+                System.out.println("\nPlanilla " + (i+1) + "[" + nombrePlanilla + "]");
 
                 for(int j = 1; j < planillaActual.size(); j++) {
                     String[] partes = planillaActual.get(j).split(": ");
@@ -421,46 +426,95 @@ public class prueba1 {
                 }
             }
 
-            System.out.print("Ingrese su opcion: ");
+            System.out.println();
 
             try {
-                    int votar = votacion.nextInt();
-                    votacion.nextLine();
-                    if (votar > 0 && votar <= planillaList.size()) {
-                        System.out.println("Esta es la planilla seleccionada, es correcta su opcion?");
-                        planillaList.get(votar -1);
+                System.out.print("Ingrese su opcion: ");
+                int voto = votacion.nextInt();
 
-                        System.out.println("1- Si");
-                        System.out.println("2- No");
-                        int voto = votacion.nextInt();
+                ArrayList<String> planillasVotada = planillaList.get(voto);
 
-                        int voto_si = voto;
+                if (voto > 0 && voto <= planillaList.size()) {
+                    System.out.println("------------------------------");
+                    System.out.println("La planillas seleccionada es: ");
+                    System.out.println("------------------------------");
 
-                        switch (voto_si) {
-                            case 1:
-                                System.out.println("Sumatoria voto");
-                                break;
-                            
-                            case 2:
-                                Votaciones();
-                                break;
-                        
-                            default:
-                                System.out.println("Ingrese una opcion valida, por favor.");
-                            break;
+                    String nombrePlanilla = planillasVotada.get(0);
+                    System.out.println("\nPlanilla: " + nombrePlanilla);
+
+                    for(int j = 1; j < planillasVotada.size(); j++) {
+                        String[] partes = planillasVotada.get(j).split(": ");
+                        if(partes.length > 1) {
+                            System.out.println("puesto" + j + " : " + partes[1]); 
+                        } else {
+                            System.out.println("puesto" + j + " : Formato incorrecto");
                         }
                     }
 
-                } catch (Exception e) {
-                    votacion.nextLine();
-                    System.out.println("Entrada inválida.");
+                    System.out.println();
+                    System.out.println("--------------------------------");
+                    System.out.println("--- Es correcta su eleccion? ---");
+                    System.out.println("--- 1- Si              ---------");
+                    System.out.println("--- 2- No              ---------");
+                    System.out.println("--------------------------------");
+                    int confirmacionVoto = votacion.nextInt();
+
+                    switch (confirmacionVoto) {
+                        case 1:
+                            Conteo_Votos(confirmacionVoto);
+                            break;
+                            
+                        case 2:
+                            Votaciones();
+                            break;
+                    
+                        default:
+                            System.out.println("Ingrese una opcion valida, por favor.");
+                        break;
+                    }
                 }
+
+            } catch (Exception e) {
+                votacion.nextLine();
+                System.out.println("Entrada inválida.");
+                @SuppressWarnings("unused")
+                String message = votacion.nextLine();
+
+                Menu();
+            }
+
         } catch (Exception e) {
             System.out.println("Ha ocurrido un error, por favor intentelo de nuevo.");
         }
     }
+
+    public static void Conteo_Votos(int numeroPlanilla) {
+        ArrayList<String> planillaVotada = planillaList.get(numeroPlanilla);
+
+        String nombrePlanilla = planillaVotada.get(0);
+        System.out.println("\nSe registró un voto para la Planilla [" + nombrePlanilla + "]");
+
+        for (int i = 1; i < planillaVotada.size(); i++) {
+            String[] partes = planillaVotada.get(i).split(": ");
+            if (partes.length == 2) {
+                String puesto = partes[0];
+                try {
+                    int votos = Integer.parseInt(partes[1].trim());
+                    votos++;
+                    // Se actualiza lista
+                    planillaVotada.set(i, puesto + ": " + votos);
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Error al leer votos del puesto: " + puesto);
+                }
+            }
+        }
+    }
+
+
+
     public static void main(String[] args){
-        Agregar_Planillas();
+        Menu();
     }
 
     public static void limpiarConsola() {
